6ead27b1114bf698038b0bbc008de1365195e651
TaturinArtem <144136367+TaturinArtem@users.noreply.github.com>
2023-09-27 16:34:52 +0500

    Task2
    Task2

---
+++ Tyuiu.TaturinAM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TaturinAM.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.TaturinAM.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Артем"));
+            Console.ReadKey();
+        }
+    }
+}
